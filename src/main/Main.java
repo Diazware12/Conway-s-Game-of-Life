2b7fccb85408ff17506744717e11f5e6fe356c0b
diazware12 <diazwalker98@gmail.com>
2021-09-18 14:19:25 +0700

    [TEST][RED] simulate in main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,17 +1,26 @@
 package main;
 
-import java.util.ArrayList;
-import java.util.Random;
+import java.util.Scanner; 
 
 import matrix.Matrix;
 
 public class Main {
 
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in); 
+		
 		Matrix foo = new Matrix();
 		foo.setRow(9);
 		foo.setCol(6);
 		foo.printMatrix();
+	    System.out.print("count enter value: ");
+
+	    int enter = scan.nextInt();  // Read user input
+	    while (enter != 0) {
+	    	String temp = scan.nextLine();
+	    	System.out.println("");
+	    	enter--;
+	    }
 	}
 
 }
--- src/matrix/Matrix.java
+++ src/matrix/Matrix.java
@@ -5,24 +5,18 @@ public class Matrix {
 	private int row;
 	private int col;
 	
-	public int getRow() {
-		return row;
-	}
-
 	public void setRow(int row) {
 		this.row = row;
-	}
-
-	public int getCol() {
-		return col;
 	}
 
 	public void setCol(int col) {
 		this.col = col;
 	}
+	
+	private Integer[][] grid = null;
 
 	public void printMatrix () {
-		Integer[][] grid = new Integer[row][col];
+		grid = new Integer[row][col];
 		for (int i = 0; i < row; i++) {
 			for (int j = 0; j < col; j++) {
 				grid[i][j] = (int)Math.round(Math.random());
@@ -33,10 +27,21 @@ public class Matrix {
 	}
 	
 	public void printNextMatrix () {
-		Integer[][] grid = new Integer[row][col];
+		Integer[][] nextMatrix = new Integer[row][col];
 		for (int i = 0; i < row; i++) {
 			for (int j = 0; j < col; j++) {
-				grid[i][j] = (int)Math.round(Math.random());
+				
+				if (i == 0 || i == col - 1 || j == 0 || j == row - 1 ) {
+					nextMatrix[i][j] = grid[i][j]; 
+				}
+				
+				
+				
+				
+				int sum = 0;
+				int neightbour = checkNeighbour(grid);
+				
+//				grid[i][j] = (int)Math.round(Math.random());
 				System.out.print(grid[i][j] + " ");
 			}
 			System.out.println();
